1bc56890712c68d00f8e1013b61064215742405f
TeamRenierb <150247356+TeamRenierb@users.noreply.github.com>
2023-11-08 17:47:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Open in WhatsApp
+
+![LICENSE](https://img.shields.io/github/license/TeamRenierb/OiWP-Bot?style=flat&color=ff0000)
+![GitHub issues](https://img.shields.io/github/issues/TeamRenierb/OiWP-Bot?color=fdf629)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/TeamRenierb/OiWP-Bot?color=c4fff9&label=Repo%20size)
+![Lines of code](https://img.shields.io/tokei/lines/github/TeamRenierb/OiWP-Bot?color=e63977)
+[![Hits-of-Code](https://hitsofcode.com/github/TeamRenierb/OiWP-Bot?branch=main)](https://hitsofcode.com/github/TeamRenierb/OiWP-Bot/view?branch=main)
+
+
+<div align="center" width="100%">
+<img width=200 src="OiWP-Logo.png">
+</div>
+
+> I was using a great [app](https://github.com/subhamtyagi/openinwa/) to open a whatsapp chat with a given number directly without saving that number in my contact list, but I felt it was overkill and uninstalled it, then searched telegram for a bot doint the same job but couldn't find so decided to make a new one.
+
+## [See it in action](https://t.me/InWhatsAppBot)
+
+
+## Deploy your own bot to vercel with one click
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FTeamRenierb%2FOiWP-Bot&env=DOMAIN,BOT_TOKEN&project-name=open-in-whatsapp&repo-name=open-in-whatsapp)
+
+### Setup webhook URL
+- Just visit this url `https://<your-app>/vercel.app/setwebhook-f443dc992ba6`
+
+## Development
+**Make sure you have Python `3.9` and [poetry](https://python-poetry.org/) installed.**
+
+- Clone this repo
+  - `git clone https://github.com/TeamRenierb/OiWP-Bot`
+  - `cd oiwa`
+  - `git checkout vercel`
+- Run
+  - `poetry shell && poetry install`
+  - `uvicorn main:app`
+
+
+###### Logo was designed by [dtafalonso](https://iconarchive.com/artist/dtafalonso.html) - [deviantart](https://www.deviantart.com/dtafalonso)
---
+++ main.py
@@ -0,0 +1,153 @@
+import logging
+import re
+from os import environ
+from random import sample
+
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse, Response
+from telegram.bot import Bot
+from telegram.chataction import ChatAction
+from telegram.parsemode import ParseMode
+from telegram.update import Update
+from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
+from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
+from telegram.error import TelegramError
+
+from dotenv import load_dotenv
+
+from helpers import logger
+
+load_dotenv()  # take environment variables from .env.
+
+DOMAIN = environ.get("DOMAIN")
+BOT_TOKEN = environ.get("BOT_TOKEN")
+
+try:
+    assert DOMAIN != None
+    assert BOT_TOKEN != None
+except AssertionError:
+    logger.critical("Please set the environment variables")
+    exit(1)
+
+try:
+    bot = Bot(BOT_TOKEN)
+except (TypeError, TelegramError):
+    logger.critical("⚠️ Invalid bot token")
+    exit(1)
+
+app = FastAPI()
+GREATINGS = [
+    "Great! Here's your link:",
+    "Sure thing! Take a look at this:",
+    "Alright! You can find your link here:",
+    "Here you go! Just click on the link below:",
+    "No problem! I've got your link ready:",
+    "Ready to go! Here's the link you need:",
+    "Perfect! Your link is just a click away:",
+    "Fantastic! You'll find your link right here:",
+    "Alrighty! Look no further, your link awaits:",
+    "Voilà! Here's the link you've been waiting for:",
+    "Ta-da! Your link is all set up and ready:",
+    "Behold! Your link is at your fingertips:",
+    "Mission accomplished! Your link is here:",
+    "Mission success! Here's the link you requested:",
+    "Drumroll, please! Your link is here:",
+    "Without further ado, here's your link:",
+    "Your wish is my command! Here's your link:",
+    "Eureka! I've found your link. Take a look:",
+    "Look what I found! Your link is right here:",
+    "And there you have it! Your link is ready to use:",
+]
+
+
+@app.get("/")
+async def index():
+    return HTMLResponse('<a href="/setwebhook">Setup webhook</a>')
+
+
+@app.get("/robots.txt")
+async def robots():
+    return HTMLResponse("User-agent: *\nDisallow: /")
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    with open("favicon.png", "rb") as f:
+        return Response(content=f.read(), media_type="image/png")
+
+
+async def cmd_start(update: Update):
+    bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Hello {update.effective_user.full_name} 👋🏻<br>Please send the phone number you want to chat with including country code",
+    )
+
+
+async def cmd_help(update: Update):
+    bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="You can send the phone number you want to chat with <b>including international code</b> (eg +917419651046)",
+        parse_mode=ParseMode.HTML,
+    )
+
+
+async def wrong_number(update: Update):
+    bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="❌ Wrong number",
+    )
+
+
+async def phone_handler(update: Update):
+    bot.send_message(
+        text=sample(GREATINGS, 1)[0],
+        chat_id=update.effective_chat.id,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="🔗 Open chat",
+                        url=f"https://api.whatsapp.com/send?phone={update.effective_message.text.replace(' ','').replace('-','').replace('+','')}&text=",
+                    )
+                ]
+            ]
+        ),
+    )
+
+
+async def update_handler(update: Update):
+    try:
+        if update.effective_message and update.effective_message.text:
+            bot.send_chat_action(
+                chat_id=update.effective_chat.id, action=ChatAction.TYPING
+            )
+            if update.effective_message.text == "/start":
+                await cmd_start(update)
+            elif update.effective_message.text == "/help":
+                await cmd_help(update)
+            elif re.fullmatch("\+[0-9\s?\-?]{5,20}", update.effective_message.text):
+                await phone_handler(update)
+            else:
+                await wrong_number(update)
+        else:
+            await cmd_help(update)
+    except (AttributeError, TelegramError) as err:
+        logging.error(f"🔴 Exception!: {err}\nupdate: {update}")
+
+
+@app.post("/telegram-update")
+async def webhook_handler(request: Request):
+    data = await request.json()
+    upcoming_update = Update.de_json(data, bot=bot)
+    await update_handler(upcoming_update)
+    return "ok"
+
+
+@app.get("/setwebhook")
+async def set_webhook():
+    s = bot.set_webhook(url=f"{DOMAIN}/telegram-update")
+    if s:
+        return HTMLResponse("ok")
+    else:
+        return HTMLResponse("Error!")
